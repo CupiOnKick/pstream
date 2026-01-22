04040aab39d4dc475f2a5f944dd3e7d2f4a80374
CupiOnKick <144584675+CupiOnKick@users.noreply.github.com>
2026-01-22 01:00:11 -0500

    Change RP name from 'P-Stream' to 'Corridor'

--- src/backend/accounts/crypto.ts
+++ src/backend/accounts/crypto.ts
@@ -221,7 +221,7 @@ export async function createPasskey(
     {
       challenge,
       rp: {
-        name: "P-Stream",
+        name: "Corridor",
         id: window.location.hostname,
       },
       user: {
