8aa8cd7846070ae30efcf38e2ed5e8fa0717d379
CupiOnKick <144584675+CupiOnKick@users.noreply.github.com>
2026-01-21 22:24:00 -0500

    config.js

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@ window.__CONFIG__ = {
   VITE_CORS_PROXY_URL: "",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OGJkMzdlOTBmYTcxYzhhNWYzZTk3YTNjMDZlODMwOCIsIm5iZiI6MTcyMDQ1OTcyNi42OTIsInN1YiI6IjY2OGMyMWNlZTMyYWUyNGRhMmJhNWI2MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IGYSNBPmnv8YvNZQGp8KAX-Y66qTbyPuzxkFTgqdg2A",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
