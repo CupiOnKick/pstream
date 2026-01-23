ce1fff52482cf0c5fe78ff6fc47e8f27c80380bd
CupiOnKick <144584675+CupiOnKick@users.noreply.github.com>
2026-01-22 22:09:13 -0500

    Update SearchFilterPopup.tsx

--- src/components/layout/SearchFilterPopup.tsx
+++ src/components/layout/SearchFilterPopup.tsx
@@ -169,7 +169,7 @@ export function SearchFilterPopup({
             onClick={onClose}
             className="p-1 hover:bg-dropdown-hoverBackground rounded transition-colors"
           >
-            <Icon icon={Icons.x} className="text-xl" />
+            <Icon icon={Icons.X} className="text-xl" />
           </button>
         </div>
 
